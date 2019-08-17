67d3b2663e04f6c4c50d4ece5fd1c564c3b36917
Olive-fcj <275464588@qq.com>
2019-08-17 10:43:33 +0800

    树的前序遍历

---
+++ lintcode/tree/InorderTraversal.py
@@ -0,0 +1,35 @@
+#-*-coding:utf-8-*-
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+class Solution:
+    """
+    @param root: A Tree
+    @return: Inorder in ArrayList which contains node values.
+    """
+    def inorderTraversal(self, root):
+        # write your code here
+        stack=[]
+        sol=[]
+        curr=root
+        while curr or stack:
+            if curr:
+                stack.append(curr)
+                curr=curr.left
+            else:
+                curr=stack.pop()
+                sol.append(curr.val)
+                curr=curr.right
+        print(sol)
+
+root = TreeNode(2)
+left = TreeNode(1)
+right = TreeNode(3)
+root.left = left
+root.right = right
+solution = Solution()
+solution.inorderTraversal(root)
+
+
